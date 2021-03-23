e23f665f99a3899e226cdd41b6e05e0d9a8a7901
HolyHades <holyhades64@gmail.com>
2021-03-23 01:54:03 +0100

    commit suicide

--- src/App.js
+++ src/App.js
@@ -125,8 +125,9 @@ function App() {
 function Home() {
 	return (
 		<div className="home">
-			<h1>idk man</h1>
-			<p>something something</p>
+			<h1>BTD6 Stats</h1>
+			<p>This is a collection of all kinds of stats for BTD6</p>
+			<p>work in progress</p>
 		</div>
 	);
 }
