4caeaa22bc93643da3846012d59460d45637d16a
HolyHades <holyhades64@gmail.com>
2021-03-24 09:38:01 +0100

    add todo

--- src/App.js
+++ src/App.js
@@ -131,6 +131,8 @@ function Home() {
 			<h1>BTD6 Stats</h1>
 			<p>This is a collection of all kinds of stats for BTD6</p>
 			<p>work in progress</p>
+			<p>things to add: tower stats, hero stats, rounds</p>
+			<p>things finished: bloon stats</p>
 		</div>
 	);
 }
