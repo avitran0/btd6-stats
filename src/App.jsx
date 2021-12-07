677a9c4a35117b24869324792baefd6c0635d620
HolyHades <holyhades64@gmail.com>
2021-12-07 09:57:56 +0100

    remove size option

--- src/App.jsx
+++ src/App.jsx
@@ -14,51 +14,10 @@ import GlueGunner from "./components/towers/glue-gunner";
 import NinjaMonkey from "./components/towers/ninja-monkey";
 import { Route, NavLink, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { useState, useRef, useEffect } from "react";
 import BTD6Logo from "./misc/BTD6Logo.png";
 
 function App() {
 	const location = useLocation();
-	const [fontSize, setFontSize] = useState(16);
-	const wrapperRef = useRef(null);
-	useOutsideAlerter(wrapperRef);
-	useEffect(() => {
-		if (localStorage.getItem("size")) setFontSize(parseInt(localStorage.getItem("size")));
-	}, [])
-	//if (localStorage.getItem("size")) setFontSize(parseInt(localStorage.getItem("size")));
-	function handleChange(type) {
-		switch (type) {
-			case "+":
-				document.documentElement.style.fontSize = fontSize + 1 + "px";
-				setFontSize(fontSize + 1);
-				localStorage.setItem("size", fontSize + 1)
-				break;
-			case "-":
-				document.documentElement.style.fontSize = fontSize - 1 + "px";
-				setFontSize(fontSize - 1);
-				localStorage.setItem("size", fontSize - 1)
-				break;
-			case "reset":
-				document.documentElement.style.fontSize = "16px";
-				setFontSize(16);
-				break;
-			default:
-				break;
-		}
-		console.log(localStorage.getItem("size"));
-	}
-	function handleModal(type) {
-		switch (type) {
-			case "open":
-				document.getElementById("modal").style.display = "flex";
-				break;
-			case "close":
-				document.getElementById("modal").style.display = "none";
-				break;
-			default:
-				break;
-		}
-	}
 	return (
 		<div className="App">
 			<header>
@@ -90,11 +49,6 @@ function App() {
 								Rounds
 							</NavLink>
 						</li>
-						<li>
-							<button onClick={() => handleModal("open")} className="font-size">
-								Size
-							</button>
-						</li>
 					</ul>
 				</nav>
 			</header>
@@ -116,32 +70,6 @@ function App() {
 					<Route path="/rounds/abr" element={<ABR />} />
 				</Routes>
 			</AnimatePresence>
-			<div id="modal">
-				<div className="modal-box" ref={wrapperRef}>
-					<h2>
-						Font Size
-						<button
-							onClick={() => handleModal("close")}
-							className="font-size"
-							style={{ marginLeft: "1rem" }}
-						>
-							&#x2715;
-						</button>
-					</h2>
-					<h2 style={{ marginBottom: 0 }}>{fontSize / 16}x</h2>
-					<div>
-						<button onClick={() => handleChange("+")} className="font-size-btn">
-							+
-						</button>
-						<button onClick={() => handleChange("reset")} className="font-size-btn">
-							&#8635;
-						</button>
-						<button onClick={() => handleChange("-")} className="font-size-btn">
-							-
-						</button>
-					</div>
-				</div>
-			</div>
 		</div>
 	);
 }
@@ -152,24 +80,6 @@ function HeroQuincy() {
 			<h1>Quincy</h1>
 		</div>
 	);
-}
-
-function useOutsideAlerter(ref) {
-	useEffect(() => {
-		function handleClickOutside(event) {
-			if (
-				ref.current &&
-				!ref.current.contains(event.target) &&
-				document.getElementById("modal").style.display === "flex"
-			) {
-				document.getElementById("modal").style.display = "none";
-			}
-		}
-		document.addEventListener("mousedown", handleClickOutside);
-		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		};
-	}, [ref]);
 }
 
 export default App;
